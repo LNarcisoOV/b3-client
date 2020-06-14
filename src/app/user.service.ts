05c8b3b7946c83013c323d21e01ff050939f2d56
Leonardo Narciso <lnarcisoov@gmail.com>
2020-06-14 15:36:44 -0300

    Creating functions to call b3 api.

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,10 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private baseUrl = "localhost:8080/b3/user";
+
+  constructor(private http: HttpClient) { }
+
+  getUser(id: number): Observable<any>{
+    return this.http.get('${this.baseUrl}/${id}');
+  }
+
+  createCustomer(customer: any): Observable<any> {
+    return this.http.post(this.baseUrl, customer);
+  }
+ 
+  updateUser(id: number, value: any): Observable<any> {
+    return this.http.put('${this.baseUrl}/${id}', value);
+  }
+ 
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete('${this.baseUrl}/${id}');
+  }
+ 
+  getUserList(): Observable<any> {
+    return this.http.get('${this.baseUrl}/');
+  }
 }
