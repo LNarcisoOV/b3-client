96941d4806c3300bdf8a206c937ac5a8e8b81009
Leonardo Narciso <lnarcisoov@gmail.com>
2020-06-14 15:32:20 -0300

    Adding route module.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+ 
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+ 
+const routes: Routes = [
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+  { path: 'user', component: UserListComponent },
+  { path: 'add', component: CreateUserComponent },
+  { path: 'findById', component: SearchUserComponent },
+];
+ 
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
