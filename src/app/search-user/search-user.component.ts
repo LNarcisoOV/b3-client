903f0dbd7211d5d5ac69b473b460e8ae7acf1714
Leonardo Narciso <lnarcisoov@gmail.com>
2020-06-14 16:25:50 -0300

    Adding functions to search user by id.

--- src/app/search-user/search-user.component.ts
+++ src/app/search-user/search-user.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
+import { User } from '../user';
+import { UserService } from '../user.service';
+ 
 @Component({
   selector: 'app-search-user',
   templateUrl: './search-user.component.html',
   styleUrls: ['./search-user.component.css']
 })
 export class SearchUserComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+ 
+  userId: number;
+  user: User[];
+ 
+  constructor(private userService: UserService) { }
+ 
+  ngOnInit() {
   }
-
+ 
+  private searchUser() {
+    this.user = [];
+    this.userService.getUser(this.userId)
+      .subscribe(user => this.user = user);
+  }
+ 
+  onSubmit() {
+    this.searchUser();
+  }
 }
